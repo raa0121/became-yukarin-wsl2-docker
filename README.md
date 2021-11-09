c9b673852969888d888a280045ed68fe8178bc0a
raa0121 <raa0121@gmail.com>
2021-11-09 23:19:04 +0900

    update

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # became-yukarin-wsl2-docker
+Windows11 では、WSL2 の docker 上から cuda を通して、GPUを利用できるため、
+[become-yukarin](https://github.com/hiroshiba/become-yukarin) をWSL2から実行する
 
 ## ビルド方法
-Windows11 上の WLS で行います
-```sh
+Windows11 上の WLS2 で行います
+```bash
 docker build -t libyukarin-dev .
-docker run --gpus all --rm -it -v $(pwd):/root/deep_yukarin libyukarin-dev bash -c 'cd deep_yukarin && pip3 install numpy===1.16.2 && pip install -r requirements.txt' 
+./setup.sh
 ```
---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -ex
+
+echo 'install python librares'
+docker run --gpus all --rm -it -v $(pwd):/root/deep_yukarin libyukarin-dev:latest \
+  bash -c 'cd /root/deep_yukarin && pip3 install numpy===1.16.2 && pip3 install -r requirements.txt'
+
+echo 'clone require repos'
+git clone https://github.com/hiroshiba/become_yukarin
+git clone https://github.com/hiroshiba/yukarin
+git clone https://github.com/YoshikazuOota/balance_sentences
+git clone https://bitbucket.org/YoshikazuOota/yukarin-tools
+
+echo 'create dat dirs'
+
+mkdir -p 1st_models_by/yukari/npy_pair/{own,target} \
+         1st_models_y/yukari/{aligned_index,aligned_wav,npy_pair,statistics} \
+         2nd_models_by/yukari \
+         input \
+         output \
+         voice_src/voice_{240,441}00/{yukarin_pair/{own,target},yukarin_single}
