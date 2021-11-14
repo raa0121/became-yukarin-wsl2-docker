57b5a8ea06841c5de941a33151cb59d7d5042a6a
raa0121 <raa0121@gmail.com>
2021-11-14 15:44:17 +0900

    fix repo mane

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ docker run --gpus all --rm -it -v $(pwd):/root/deep_yukarin libyukarin-dev:lates
   bash -c 'cd /root/deep_yukarin && pip3 install numpy===1.16.2 && pip3 install -r requirements.txt'
 
 echo 'clone require repos'
-git clone https://github.com/hiroshiba/become_yukarin
+git clone https://github.com/hiroshiba/become-yukarin
 git clone https://github.com/hiroshiba/yukarin
 git clone https://github.com/YoshikazuOota/balance_sentences
 git clone https://bitbucket.org/YoshikazuOota/yukarin-tools
